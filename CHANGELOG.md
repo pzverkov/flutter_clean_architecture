b7e0d70766ea5bf497eee0c81d604baa21a66a28
peter.zverkov888@gmail.com <peter.zverkov888@gmail.com>
2020-02-13 19:52:23 +0100

    initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,104 @@
+## [3.0.2] - Wednesday, January 8th, 2020
+
+- OnResumed bug fix
+
+## [3.0.1] - Wednesday, January 8th, 2020
+
+- Fixed typo in documentation.
+
+## [3.0.0] - Wednesday, January 8th, 2020
+### What's New
+- Improves performance of the library by using the [Provider package](https://pub.dev/packages/provider) internally.
+- Added an option to enable debug mode via `FlutterCleanArchitecture.debugModeOn()`
+- Added the ability to use a common `Controller` for widgets that exist within a page.
+The widgets can access the `Controller` in the tree via the `FlutterCleanArchitecture.getController<Controller>(context)` method.
+
+### Breaking Changes
+- `ViewState`'s `build()` method can no longer be overridden.
+- `ViewState` UI code must now go into `buildPage()` method.
+- `callHandler()` is now removed
+- Removed `loadOnStart()`
+- Removed `startLoading()`
+- Removed `dismissLoading()`
+
+## [2.0.2] - Thursday, December 26th, 2019
+
+* Fixed an issue where `Controller.getContext()` throws due to losing context because lifecycle events where triggered after the `View` had already been dismounted.
+
+## [2.0.1] - Thursday, December 26th, 2019
+
+* Fixed error in docs
+
+## [2.0.0] - Thursday, December 26th, 2019
+
+* Updated RxDart dependency
+* Breaking changes
+  * All Observable return types are now changed to Streams
+* Updated documentation
+
+## [1.1.0] - Tuesday, December 17th, 2019
+
+* Added the ability to create a usecase that executes on a different isolate.
+    * `BackgroundUseCase`
+        * Usecase class that executes on a different isolate
+        * Create a static method in the class that conforms to `UseCaseTask` signature
+        * Return the reference to that method inside the `buildUseCaseTask` method
+    * `BackgroundUseCaseParameters`
+        * Contains the input data provided to to the isolate
+        * Passed to the static method as a parameter
+    * `BackgroundUseCaseMessage`
+        * Provides output data and information such as completion status and errors
+    * The usecase usage in the presenter does not change, this class can be used identically
+    * Check `README.md` for more details
+* Added and improved documentation
+* Updated README.md
+
+## [1.0.8] - Tuesday, December 12th, 2019
+
+* Bug fix
+
+## [1.0.7] - Tuesday, December 12th, 2019
+
+* Bug fix
+
+## [1.0.6] - Tuesday, December 11th, 2019
+
+* Flutter v1.12.5 compatibility
+* Changed onSuspending to onDetatched
+
+## [1.0.5] - Tuesday, August 27th, 2019
+
+* Updated dependencies
+
+## [1.0.4] - Tuesday, August 27th, 2019
+
+* Updated dependencies
+
+## [1.0.3] - Tuesday, July 2nd, 2019
+
+* Add missing files to example
+
+## [1.0.2] - Thursday, May 9th, 2019
+
+* Fixed documentation typos
+* Changed misleading assertion messages
+* Cleaned documentation
+
+## [1.0.1] - Tuesday, April 30th, 2019
+
+* Fixed documentation typos
+
+## [1.0.0] - Tuesday, April 30th, 2019
+
+* Added example application
+* Addded documentation
+
+## [0.0.1] - Tuesday, April 30th, 2019
+
+* Implementation of the Clean Architecture by Uncle Bob in Flutter Library
+  * View class
+  * Controller class
+  * Presenter class
+  * UseCase class
+  * Observer class
+* Can be used to set up a Flutter project using the Clean Architecture
---
+++ lib/src/view.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_clean_architecture/src/controller.dart';
+import 'package:logging/logging.dart';
+import 'package:meta/meta.dart';
+import 'package:provider/provider.dart';
+
+/// The [ViewState] represents the [State] of a [StatefulWidget], typically of a screen or a
+/// page. The [ViewState] requires a [Controller] to handle its events and provide its data.
+///
+/// The [ViewState] also has a default [globalKey] that can be used inside its `build()` function
+/// in a widget to grant easy access to the [Controller], which could then use it to display
+/// snackbars, dialogs, and so on.
+///
+/// The [ViewState] lifecycle is also handled by the [Controller].
+/// ```dart
+///     class CounterState extends ViewState<CounterPage, CounterController> {
+///       CounterState(CounterController controller) : super(controller);
+///
+///       @override
+///       Widget build(BuildContext context) {
+///         return MaterialApp(
+///           title: 'Flutter Demo',
+///           home: Scaffold(
+///             key: globalKey, // using the built-in global key of the `View` for the scaffold or any other
+///                             // widget provides the controller with a way to access them via getContext(), getState(), getStateKey()
+///             body: Column(
+///               children: <Widget>[
+///                 Center(
+///                   // show the number of times the button has been clicked
+///                   child: Text(controller.counter.toString()),
+///                 ),
+///                 // you can refresh manually inside the controller
+///                 // using refreshUI()
+///                 MaterialButton(onPressed: controller.increment),
+///               ],
+///             ),
+///           ),
+///         );
+///       }
+///     }
+///
+/// ```
+abstract class ViewState<Page extends View, Con extends Controller>
+    extends State<Page> {
+
+  final GlobalKey<State<StatefulWidget>> globalKey =
+      GlobalKey<State<StatefulWidget>>();
+  Con _controller;
+  Logger _logger;
+  Con get controller => _controller;
+  ViewState(this._controller) {
+    _controller.initController(globalKey);
+    WidgetsBinding.instance.addObserver(_controller);
+    _logger = Logger('${this.runtimeType}');
+  }
+
+  @override
+  @mustCallSuper
+  void didChangeDependencies() {
+    if (widget.routeObserver != null) {
+      _logger.info('$runtimeType is observring route events.');
+      widget.routeObserver.subscribe(_controller, ModalRoute.of(context));
+    }
+
+    super.didChangeDependencies();
+  }
+
+  Widget buildPage();
+
+  @override
+  @nonVirtual
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<Con>.value(
+        value: _controller,
+        child: Consumer<Con>(builder: (ctx, con, _) {
+          _controller = con;
+          return buildPage();
+        }));
+  }
+
+  @override
+  @mustCallSuper
+  void dispose() {
+    _logger.info('Disposing $runtimeType.');
+    _controller.dispose();
+    super.dispose();
+  }
+}
+
+/// The [View] represents a [StatefulWidget]. The [View] is typically a page or screen in
+/// the application. However, a [View] can be any [StatefulWidget]. The [View] must have a
+/// [State], and that [State] should be of type [ViewState<MyView, MyController>].
+///
+/// If a [RouteObserver] is given to the [View], it is used to register its [Controller] as
+/// a subscriber, which provides the ability to listen to push and pop route events.
+/// ```dart
+///   class CounterPage extends View {
+///     CounterPage({RouteObserver observer, Key key}): super(routeObserver: routeObserver, key: key);
+///     @override
+///     // Dependencies can be injected here
+///     State<StatefulWidget> createState() => CounterState(Controller());
+///   }
+///
+/// ```
+///
+abstract class View extends StatefulWidget {
+  final RouteObserver routeObserver;
+  final Key key;
+  View({this.routeObserver, this.key}) : super(key: key);
+}
